7f842f6d8844782187a2f2218906fd859e248efe
zippyh <zippyh@icloud.com>
2026-02-21 00:39:24 -0500

    working on block search

--- search/BlockSearchProblem.java
+++ search/BlockSearchProblem.java
@@ -7,6 +7,10 @@ public class BlockSearchProblem extends Problem {
         super(startOrder);
     }
 
+    public BlockSearchProblem(State startOrder, State goalOrder) {
+        super(startOrder, goalOrder);
+    }
+
     @Override
     public ArrayList<Tuple> successor(State state) {
         ArrayList<Tuple> successors = new ArrayList<>();
@@ -17,13 +21,13 @@ public class BlockSearchProblem extends Problem {
                 blankIndex = i;
             }
         }
-        //List of moves
+        // List of moves
         ArrayList<String> moves = new ArrayList<>();
         moves.add("UP");
         moves.add("LEFT");
         moves.add("RIGHT");
         moves.add("DOWN");
-        //Makes a new state for every move
+        // Makes a new state for every move
         for (String move : moves) {
             char[] newState = charState.clone();
             switch (move) {
@@ -44,7 +48,7 @@ public class BlockSearchProblem extends Problem {
                     }
                     break;
                 case "RIGHT":
-                    if(blankIndex % 3 != 2) {
+                    if (blankIndex % 3 != 2) {
                         char temp = newState[blankIndex + 1];
                         newState[blankIndex + 1] = '0';
                         newState[blankIndex] = temp;
@@ -61,6 +65,22 @@ public class BlockSearchProblem extends Problem {
                     break;
             }
         }
-        return null;
+        return successors;
     }
+
+    @Override
+    public double h(Node node) {
+        String currentState = node.getState().toString();
+        String goalState = this.goal.toString();
+        int distance = 0;
+        for (int i = 0; i < currentState.length(); i++) {
+            char c = currentState.charAt(i);
+            if (c != '0') {
+                int goalIndex = goalState.indexOf(c);
+                distance += Math.abs(i / 3 - goalIndex / 3) + Math.abs(i % 3 - goalIndex % 3);
+            }
+        }
+        return distance;
+    }
+
 }
